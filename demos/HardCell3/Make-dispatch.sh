ed514b10501c9ce06a9051df1d425343d084158f
Dave Ackley <ackley@livingcomputation.org>
2022-06-23 02:10:53 -0600

    Rework demo makefiles again
    for possibly actually working on the T2s and mfms

--- demos/HardCell3/Make-dispatch.sh
+++ demos/HardCell3/Make-dispatch.sh
@@ -4,18 +4,35 @@ ARCH=`uname -m`
 NODE=`uname -n`
 if [ "x$ARCH" == "xarmv7l" -a "x$NODE" == "xbeaglebone" ] ; then
     ENGINE=t2
-    CDMD=ok
+    DEFAULT_ULAM_BIN=/home/t2/ULAM/bin
+    DEFAULT_MFM_BIN=/home/t2/MFM/bin
 else
     ENGINE=mfms
+    DEFAULT_ULAM_BIN=/usr/bin
+    DEFAULT_MFM_BIN=/usr/bin
 fi
 echo BUILDING FOR ENGINE: $ENGINE
-if [ "x$MAKEARG" == "xcdmd" ] ; then
-    if [ "x$ENGINE" == "xmfms" ] ; then
-        echo "Can't '$MAKEARG' on '$ENGINE'"
-        exit 1
-    else
-        make -k -f Makefile-t2.mk cdmd
-    fi
-else
-    make -C code -k -f Makefile-$ENGINE.mk $MAKEARG
+
+MAKEVARS+=" ENGINE=$ENGINE"
+MAKEVARS+=" DEFAULT_ULAM_BIN=$DEFAULT_ULAM_BIN"
+MAKEVARS+=" DEFAULT_MFM_BIN=$DEFAULT_MFM_BIN"
+
+# Peel off some special cases
+TARGENG="$ENGINE-$MAKEARG"
+
+# cdmd is invalid on mfms
+if [ "x$TARGENG" == "xmfms-cdmd" ] ; then
+    echo "Can't '$MAKEARG' on '$ENGINE'"
+    exit 1
 fi
+
+# cdmd and install on t2 runs at ., not ./code    
+if [ "x$TARGENG" == "xt2-cdmd" -o  "x$TARGENG" == "xt2-install" ] ; then
+    make -C . -k -f Makefile-$ENGINE.mk $MAKEARG $MAKEVARS
+    exit $?
+fi
+
+# everything else runs via ./code/Makefile
+echo make -C code -k $MAKEARG $MAKEVARS
+make -C code -k $MAKEARG $MAKEVARS
+
